9025e9a4080ab86057d65902e55cd4338580fa64
kiparenkomb <kiparenkomb@gmail.com>
2019-09-25 23:51:46 +0300

    added TodoList component

---
+++ src/components/TodoList/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './style.css';
+
+function TodoList() {
+
+    return (
+        <ul className="TodoList">
+            TodoList
+        </ul>
+    )
+}
+
+export default TodoList;
